54766f52479d9b2bd90c596c54801f04c4e94736
Lenovo <Lenovo@DESKTOP-65NLRJP>
2021-11-16 11:32:59 +0300

    Add project files.

---
+++ Kaberdin_LB1/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace Kaberdin_LB1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateHostBuilder(string[] args)
+        {
+            var port = Environment.GetEnvironmentVariable("PORT");
+            if(port == null)
+            {
+                return WebHost.CreateDefaultBuilder(args)
+                    .UseStartup<Startup>();
+            }
+            return WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseUrls("http://*:" + port);
+        }
+    }
+}
